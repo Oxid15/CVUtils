bf75b3f17c06219aace0e67d8b468f96f8095857
Ilia Moiseev <villeman.5@yandex.ru>
2020-10-10 16:05:32 +0500

    idk

--- cvutils.py
+++ cvutils.py
@@ -1,25 +1,19 @@
-import os
+from cvutils_import import *
 
-import numpy as np
-import cv2
-from matplotlib import pyplot as plt
-from PIL import Image
-
-from skimage.measure import compare_ssim as ssim
-from skimage.measure import compare_psnr as psnr
-
-def show_one(img, figsize):
+def show_one(img, figsize, cmap):
     plt.figure(figsize=figsize)
     plt.imshow(img)
     plt.show()
 
-def show_multiple(imgs, shape, figsize, max_img):
+def show_multiple(imgs, shape, figsize, max_img, cmap):
     k = 0
+    if(max_img is None):
+        max_img = len(imgs)
 
     if(shape is None):
-        plt.figure(figsize=figsize)
         for img in imgs:
             if(k < max_img):
+                plt.figure(figsize=figsize)
                 plt.imshow(img)
                 plt.show()
                 k += 1
@@ -28,8 +22,8 @@ def show_multiple(imgs, shape, figsize, max_img):
         if(shape[0] != 1 and shape[1] != 1):
             for i in range(shape[0]):
                 for j in range(shape[1]):
-                    if(k < imgs.shape[0] or k < max_img):
-                        ax[i][j].imshow(imgs[k])
+                    if(k < max_img):
+                        ax[i][j].imshow(imgs[k], cmap=cmap)
                         k += 1
         else:
             if(shape[0] == 1):
@@ -37,18 +31,18 @@ def show_multiple(imgs, shape, figsize, max_img):
             elif(shape[1] == 1):
                 opposite_shape = 0
             for i in range(shape[opposite_shape]):
-                if(k < imgs.shape[0] or k < max_img):
-                    ax[i].imshow(imgs[k])
+                if(k < max_img):
+                    ax[i].imshow(imgs[k], cmap=cmap)
                     k += 1
         plt.show()
 
-def show_from(path, shape, figsize, max_img):
+def show_from(path, shape, figsize, max_img, cmap):
     if(os.path.isdir(path)):
         imgs = read_images(path, max_img=max_img)
-        show_multiple(imgs, shape, figsize, max_img)
+        show_multiple(imgs, shape, figsize, max_img, cmap)
     else:
         img = np.array(Image.open(path))
-        imshow(img, figsize=figsize)       
+        imshow(img, figsize=figsize, cmap=cmap)       
 
 # def read_images_sort(path):
 #     images = []
@@ -64,28 +58,31 @@ def show_from(path, shape, figsize, max_img):
 #     return np.array(images)
 
 def read_images(path, max_img):
-    k = 0
     images = []
     filenames = os.listdir(path)
-    for name in filenames:
-        if(k < max_img):
-            try:
-                image = Image.open(os.path.join(path, name))
-            except Exception:
-                pass
-            else:
-                images.append(np.array(image))
-                k += 1
+    if(max_img is None):
+        max_img = len(filenames)
+    for i in range(max_img):
+        try:
+            image = Image.open(os.path.join(path, filenames[i]))
+        except Exception:
+            pass
+        else:
+            images.append(np.array(image))
     return np.array(images)
 
-def imshow(arg, shape=None, figsize=None, max_img=None):
+def imshow(arg, grayscale=False, shape=None, figsize=None, max_img=None, cmap=None):
+    if arg is None:
+        raise ValueError("The argument was None!")
     if(type(arg) == str):
-        show_from(arg, shape, figsize, max_img)
+        show_from(arg, shape, figsize, max_img, cmap)
     else:
-        if(len(arg.shape) <= 3):
-            show_one(arg, figsize)
-        elif(len(arg.shape) == 4):
-            show_multiple(arg, shape, figsize, max_img)
+        if(type(arg) == list):
+            arg = np.array(arg)
+        if(grayscale and len(arg.shape) == 3 or len(arg.shape) == 4):
+            show_multiple(arg, shape, figsize, max_img, cmap)
+        elif(len(arg.shape) <= 3):
+            show_one(arg, figsize, cmap)
 
 def add_noise_gaussian(img, mean, std, grayscale=False):
     if(grayscale):
@@ -94,3 +91,13 @@ def add_noise_gaussian(img, mean, std, grayscale=False):
     else:
         noisy_img = img + np.random.normal(mean, std, img.shape)
     return np.clip(noisy_img, 0, 255).astype(np.uint8)
+
+def open(path):
+    return np.array(Image.open(path))
+
+def normalize_to_255(image):
+    _max = np.nanmax(image)
+    return ((image / _max) * 255).astype(np.uint8)
+
+def normalize_to_1(image):
+    return image / np.nanmax(image)
