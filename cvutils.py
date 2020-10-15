ae623adb34881541cfca43f8006623ea77054eb3
Ilia Moiseev <villeman.5@yandex.ru>
2020-10-15 17:21:32 +0500

    documentation added

--- cvutils.py
+++ cvutils.py
@@ -18,7 +18,7 @@ def _is_multiple(img):
         elif(len(shape) == 4): # it is an array of 3-channel images (n, h, w, 3)
             return True
 
-def _show_one(img, figsize, cmap, title, suptitle):
+def _show_one(img, figsize, cmap_name, title, suptitle):
     if(figsize == 'auto'):
         figsize = None
     _, ax = plt.subplots(figsize=figsize)
@@ -26,29 +26,29 @@ def _show_one(img, figsize, cmap, title, suptitle):
         ax.set_title(title)
     plt.suptitle(suptitle)
     ax.set_title(title)
-    ax.imshow(img, cmap=cmap)
+    ax.imshow(img, cmap=plt.cm.get_cmap(cmap_name))
     plt.show()
 
-def _show_consequent(imgs, shape, figsize, max_img, cmap, titles, figsize_factor, suptitle):
+def _show_consequent(imgs, shape, figsize, max_img, cmap_name, titles, figsize_factor, suptitle):
     k = 0
     if(shape is None):
         for img in imgs:
             if(k < max_img):
                 _, ax = plt.subplots(figsize=figsize)
-                ax.imshow(img)
+                ax.imshow(img, cmap=plt.cm.get_cmap(cmap_name))
                 if(titles is not None):
                     ax.set_title(titles[k])
                 plt.show()
                 k += 1
 
-def _show_on_one_plot(imgs, shape, figsize, max_img, cmap, titles, figsize_factor, suptitle):
+def _show_on_one_plot(imgs, shape, figsize, max_img, cmap_name, titles, figsize_factor, suptitle):
     k = 0
     _, ax = plt.subplots(shape[0], shape[1], figsize=figsize)
     if(shape[0] != 1 and shape[1] != 1):
         for i in range(shape[0]):
             for j in range(shape[1]):
                 if(k < max_img):
-                    ax[i][j].imshow(imgs[k], cmap=cmap)
+                    ax[i][j].imshow(imgs[k], cmap=plt.cm.get_cmap(cmap_name))
                     if(titles is not None):
                         ax[i][j].set_title(titles[k])
                     k += 1
@@ -59,12 +59,12 @@ def _show_on_one_plot(imgs, shape, figsize, max_img, cmap, titles, figsize_facto
             opposite_shape = 0
         for i in range(shape[opposite_shape]):
             if(k < max_img):
-                ax[i].imshow(imgs[k], cmap=cmap)
+                ax[i].imshow(imgs[k], cmap=plt.cm.get_cmap(cmap_name))
                 if(titles is not None):
                         ax[i].set_title(titles[k])
                 k += 1
 
-def _show_multiple(imgs, shape, figsize, max_img, cmap, titles, figsize_factor, suptitle):
+def _show_multiple(imgs, shape, figsize, max_img, cmap_name, titles, figsize_factor, suptitle):
     plt.suptitle(suptitle)
     
     if(max_img is None):
@@ -74,18 +74,18 @@ def _show_multiple(imgs, shape, figsize, max_img, cmap, titles, figsize_factor, 
     if(figsize == 'auto'):
         figsize = _calculate_figsize(imgs, shape, figsize_factor)
         if(shape is None):
-            _show_consequent(imgs, shape, figsize, max_img, cmap, titles, figsize_factor, suptitle)
+            _show_consequent(imgs, shape, figsize, max_img, cmap_name, titles, figsize_factor, suptitle)
         else:
-            _show_on_one_plot(imgs, shape, figsize, max_img, cmap, titles, figsize_factor, suptitle)
+            _show_on_one_plot(imgs, shape, figsize, max_img, cmap_name, titles, figsize_factor, suptitle)
         plt.show()
 
-def _show_from(path, shape, figsize, max_img, cmap, titles, figsize_factor, suptitle):
+def _show_from(path, shape, figsize, max_img, cmap_name, titles, figsize_factor, suptitle):
     if(os.path.isdir(path)):
         imgs = imread(path, max_img=max_img)
-        _show_multiple(imgs, shape, figsize, max_img, cmap, titles, figsize_factor, suptitle)
+        _show_multiple(imgs, shape, figsize, max_img, cmap_name, titles, figsize_factor, suptitle)
     else:
         img = np.array(Image.open(path))
-        imshow(img, figsize=figsize, max_img=max_img, cmap=cmap, titles=titles, figsize_factor=figsize_factor, suptitle=suptitle)   
+        imshow(img, figsize=figsize, max_img=max_img, cmap_name=cmap_name, titles=titles, figsize_factor=figsize_factor, suptitle=suptitle)   
 
 def _factorize(num):
     factors = []
@@ -127,23 +127,62 @@ def _calculate_figsize(imgs, shape, factor):
     ratio = sum_height / sum_width
     return (factor, factor * ratio)
 
-def imshow(arg, shape='auto', figsize='auto', figsize_factor=None, max_img=None, cmap=None, titles=None, suptitle=None):
+def imshow(arg, shape='auto', figsize='auto', figsize_factor=None, max_img=None, cmap_name=None, titles=None, suptitle=None):
     '''
     Displays an image or a sequence of images.
+      
+    Parameters:  
+    arg: (string, np.ndarray, list, tuple) can be a path to a folder or a file or np.array containing image or sequence of images; if it is a folder path, reads 
+    all images from folder, or a certain amount that is set by max_img argument    
+      
+    shape: default is 'auto', (tuple) if multiple images are passed, determines a shape with which they will be shown in form (rows, columns);
+    if set to 'auto' calculates the optimal shape in which images should be shown considering their quantity and aspect ratio; pass 
+    tuple to set custom shape; if shape is set for the more images that were passed i.e. (3, 2) for 5 images, it would left the one subplot blank;
+    would let one to be blank automatically in 'auto' setting if the number of images passed is prime  
+       
+    figsize: default is 'auto'; (tuple) figsize argument from matplotlib to set the size of the plot; if set to 'auto' calculates the figsize that would 
+    be optimal considering shape and image aspect ratios  
+      
+    figsize_factor: (int) set this if figsize is set to 'auto'; represents the width of the figure that is passsed to matplotlib; the height is computed by multiplying this value by aspect ratio calculated from shape  
+      
+    max_img: (int) sets the maximum number of images to read if arg is the path to folder  
+      
+    cmap_name: (str) the name of colormap from matplotlib  
+      
+    titles: (list of str) a list of titles for the subplots to the set_title  
+      
+    suptitle: (str) the title for the whole plot  
+      
+    Returns: None
     '''
     if arg is None:
         raise ValueError("The argument was None!")
     if(type(arg) == str):
-        _show_from(arg, shape, figsize, max_img, cmap, titles, figsize_factor, suptitle)
+        _show_from(arg, shape, figsize, max_img, cmap_name, titles, figsize_factor, suptitle)
     else:
         if(type(arg) == list):
             arg = np.array(arg)
         if(_is_multiple(arg)):
-           _show_multiple(arg, shape, figsize, max_img, cmap, titles, figsize_factor, suptitle)
+           _show_multiple(arg, shape, figsize, max_img, cmap_name, titles, figsize_factor, suptitle)
         else:
-           _show_one(arg, figsize, cmap, titles, suptitle)
+           _show_one(arg, figsize, cmap_name, titles, suptitle)
 
-def add_noise_gaussian(img, mean=0, std=1, grayscale=False):
+def add_noise_gaussian(img, grayscale, mean=0, std=1):
+    '''
+    Adds gaussian noise with mean and standard deviation then clips to [0, 255] interval.  
+      
+    Parameters:  
+      
+    img: (np.ndarray) input image  
+      
+    grayscale: (bool) whether image is grayscale  
+      
+    mean: (float) mean for the gaussian  
+      
+    std: (float) standard deviation for the gaussian  
+      
+    Returns: (np.ndarray) new image with noise
+    '''
     if(grayscale):
         noise = np.random.normal(mean, std, (img.shape[0], img.shape[1]))
         noisy_img = img + np.dstack((noise, noise, noise))
@@ -176,24 +215,73 @@ def _read_one(path, shape):
         return _resize_one(np.array(Image.open(path)), shape)
 
 def imread(path, max_img=None, shape=None):
+    '''
+    Reads an image or a list of images. Uses PIL: Image.open()  
+      
+    Parameters:  
+      
+    path: (str) path to the file or folder  
+      
+    max_img: (int) maximum number of images  
+      
+    shape: (tuple) the desired shape for images to be resized; does not resize anything by default, when is set calls cv2.resize with shape
+      
+    Returns: (np.ndarray) image or list of images
+    '''
     if(os.path.isdir(path)):
         return _read_batch(path, shape)
     else:
         return _read_one(path, shape)
 
 def open_and_show(path):
-    img = open(path)
-    _show_one(img, (10,10), plt.cm.get_cmap('viridis'), 'input', 'input')
+    '''
+    Reads singular image and shows it by matplotlib.  
+      
+    Parameters:  
+      
+    path (str): path to the image file  
+      
+    Returns: (np.ndarray) image
+    '''
+    img = imread(path)
+    _show_one(img, (10,10), 'viridis', None, 'input')
     return img
 
 def normalize(img):
+    '''
+    Normalizes the image by subtracting the mean and dividing by the standard deviation.  
+      
+    Parameters:  
+      
+    img - image to be normalized  
+      
+    Returns:  (np.ndarray) normalized image
+    '''
     return (img - np.mean(img)) / np.std(img)
 
-def to_255(image):
-    _max = np.nanmax(image)
-    return ((image / _max) * 255).astype(np.uint8)
+def to_255(img):
+    '''
+    Normalizes image to [0, 255] by dividing by the maximum value and multiplying by 255. Ignores nans by np.nanmax(). Casts the dtype to uint8.
+      
+    Parameters:  
+      
+    img - image to be normalized  
+      
+    Returns:  (np.ndarray) normalized image of dtype uint8
+    '''
+    _max = np.nanmax(img)
+    return ((img / _max) * 255).astype(np.uint8)
 
 def to_1(image):
+    '''
+    Normalizes image to [0, 1] by dividing by the maximum value. Ignores nans by np.nanmax().
+      
+    Parameters:  
+      
+    img - image to be normalized  
+      
+    Returns:  (np.ndarray) normalized image
+    '''
     return image / np.nanmax(image)
 
 def _save_one(img, path, name):
@@ -204,6 +292,19 @@ def _save_one(img, path, name):
         raise FileNotFoundError('file {} is not found'.format(os.path.join(path, name))) from BaseException
 
 def save(imgs, path, names):
+    '''
+    Saves images to the disk.  
+      
+    Parameters:  
+      
+    imgs: (np.ndarray, list) singular image or a list of images  
+      
+    path: (str) path for saving  
+      
+    names: (list) names of the files; use generator expressions if you have no list of names  
+      
+    Returns: None
+    '''
     if(_is_multiple(imgs)):
         for i in range(len(imgs)):
             _save_one(imgs[i], path, names[i])
@@ -215,7 +316,24 @@ def _add_suffixes(names, suffixes):
         name, ext = os.path.splitext(names[i])
         names[i] = name + str(suffixes[i]) + ext
 
-def apply(imgs, func, save_path=None, names=None, verbose=False): #TODO: add support of arg being path to folder
+def apply(imgs, func, path=None, names=None, verbose=False): #TODO: add support of arg being path to folder
+    '''
+    Applies function to the images. Can automatically save images when done.   
+      
+    Parameters:  
+      
+    imgs: (np.ndarray, list) list of images  
+      
+    func: (function) function to be applied to images `result = func(img)`  
+      
+    path: (str) path to folder to save the results
+      
+    names: (list of str) names of the files; use generator expressions if you have no list of names  
+      
+    verbose: (bool) False by default: enables indication of progress with time measurement  
+      
+    Returns: (np.ndarray) results even if saves to folder
+    '''
     results = []
     num = 1
     size = len(imgs)
@@ -226,20 +344,30 @@ def apply(imgs, func, save_path=None, names=None, verbose=False): #TODO: add sup
         end = time.time()
         if(verbose):
             print('{}/{} processed in {:.2f}s '.format(num, size, end-start), end= '')
-            if(save_path is not None):
+            if(path is not None):
                 print('')
         
-        if(save_path is not None):
+        if(path is not None):
             start = time.time()
-            save(result, save_path, names[num-1])
+            save(result, path, names[num-1])
             end = time.time()
             if(verbose):
                 print('saved in {:.2f}s'.format(end-start))
         num += 1
-
     return results
 
 def percentile(array):
+    '''
+    Gives list of percentiles of array.  
+      
+    Parameters:  
+      
+    array: (np.ndarray) array to get percentiles from  
+      
+    Returns: (pandas.DataFrame) DataFrame with the following percentiles of input array: [0, 25 , 50, 75, 100]. 
+    Useful function to call in Jupyter notebooks to fast check the distribution of the particular array. 
+    Pandas DataFrame isn't necessary, but it gives nice output in the form of table.  
+    '''
     return pd.DataFrame(np.percentile(array, [0, 25, 50, 75, 100]), index=['0', '25', '50', '75', '100']).T
 
 def _resize_batch(imgs, shape):
@@ -254,17 +382,28 @@ def _resize_one(img, shape):
     return cv2.resize(img, shape)
 
 def resize(imgs, shape):
+    '''
+    Resizes image or a list of images.    
+      
+    Parameters:  
+      
+    imgs: (np.ndarray, list) image or a list of images  
+      
+    shape: shape that is passed to `cv2.resize()` function  
+      
+    Returns: (np.ndarray) resized images  
+    '''
     if(_is_multiple(imgs)):
         return _resize_batch(imgs, shape)
     else:
         return _resize_one(imgs, shape)
 
-def historyplot(history, figsize=(10,10)):
-    plt.figure(figsize=figsize)
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'val'], loc = 'upper left')
-    plt.show()
+# def historyplot(history, figsize=(20,10)): #TODO: add support for different metrics
+#     plt.figure(figsize=figsize)
+#     plt.plot(history.history['loss'])
+#     plt.plot(history.history['val_loss'])
+#     plt.title('model loss')
+#     plt.ylabel('loss')
+#     plt.xlabel('epoch')
+#     plt.legend(['train', 'val'], loc = 'upper left')
+#     plt.show()
